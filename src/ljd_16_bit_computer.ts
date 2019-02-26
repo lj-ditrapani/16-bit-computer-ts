ac9b1a118cd40a4d4e8fc9d16d225b8a8dd2b93b
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-02-25 22:14:26 -0500

    Load program & data ROMs from binary file

--- src/ljd_16_bit_computer.ts
+++ src/ljd_16_bit_computer.ts
@@ -1,49 +1,27 @@
+import * as fs from 'fs'
 import { keyCodes, makeTermGrid } from 'term-grid-ui'
-import { makeDebugCpu } from '/home/ljd/fun/16-bit-cpu-ts'
+import { Cpu } from '/home/ljd/fun/16-bit-cpu-ts'
+
+const file = fs.readFileSync('test.bin')
+
+const programRom = new Uint16Array(64 * 1024)
+const dataRom = new Uint16Array(32 * 1024)
+
+let fileIndex = 0
+for (let programIndex = 0; programIndex < 64 * 1024; programIndex++) {
+  programRom[programIndex] = (file[fileIndex++] << 8) | file[fileIndex++]
+}
+
+for (let dataIndex = 0; dataIndex < 32 * 1024; dataIndex++) {
+  dataRom[dataIndex] = (file[fileIndex++] << 8) | file[fileIndex++]
+}
 
 const tg = makeTermGrid(15, 32)
 
-const programRom = [
-  0x1f8a, // $F840 -> RA (color ram)
-  0x240a, //
-  0x10a1, // $0a04 -> R1 (medium cyan / dark green)
-  0x2041,
-  0x4a10, // STR $0A04 -> mem[$F840]
-  0x241a, // $F841 -> RA
-  0x1031, // $032B -> R1 (blue / light blue)
-  0x22b1, //
-  0x4a10, // STR $032B -> mem[$F841]
-  0x247a, // $F847 -> RA (end of color ram)
-  0x1001, // $0030 -> R1 (black / red)
-  0x2301, //
-  0x4a10, // STR $0030 -> mem[$F847]
-  0x100a, // HBY 0x00 RA
-  0x200a, // LBY 0x00 RA
-  0x3a01, // LOD RA R1
-  0x201a, // LBY 0x01 RA
-  0x3a02, // LOD RA R2
-  0x5123, // ADD R1 R2 R3
-  0x1013, // set fg & bg color indexes (medium cyan / dark green)
-  0x1faa, // HBY $FA00 -> RA (screen ram)
-  0x200a, // LBY
-  0x4a30, // STR $0164 -> mem[$FA00]
-  0x1211, // HBY $2300 -> R1 (blue / dark green)
-  0x2001,
-  0x202a, // LBY
-  0x4a10, // STR $2100 -> mem[$FA02]
-  0x1fba, // HBY $FBDF -> RA (screen ram)
-  0x2dfa, //
-  0x4a30, // STR $0164 -> mem[$FBDF]
-  0x2c0a, // LBY $FBC0 -> RA (screen ram)
-  0x1fe3, // set fg & bg color indexes
-  0x4a30, // STR $0164 -> mem[$FBDF]
-  0x0000 // END
-]
-
-const dataRom = [27, 73, 0]
-const cpuAndIoRam = makeDebugCpu(programRom, dataRom)
-const cpu = cpuAndIoRam.cpu
-let ioRam = cpuAndIoRam.ioRam
+const ioRam1: Uint16Array = new Uint16Array(1 * 1024)
+const ioRam2: Uint16Array = new Uint16Array(1 * 1024)
+const cpu = new Cpu(programRom, dataRom, ioRam1, ioRam2)
+let ioRam = ioRam2
 
 function* range(end: number) {
   for (let i = 0; i < end; i += 1) {
